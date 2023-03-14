28e64c870b796151b9dc33cbf056d99e32efa38b
bijish-personal <bijish_js@outlook.com>
2023-03-15 01:31:22 +0530

    header and exprerience section created

---
+++ src/data/changeableData.ts
@@ -0,0 +1,35 @@
+export const experienceData = [
+    {
+        jobPosition: "Team Lead",
+        companyName: "Credot",
+        dateStarted: "October 2022",
+        dateEnded: "PRESENT",
+        descriptionPoints: [
+            "Lead a team, created full stack applications",
+            "Lead a team",
+            "Lead a team",
+            "Lead a team",
+        ]
+    },
+    {
+        jobPosition: "Full stack developer",
+        companyName: "Credot",
+        dateStarted: "May 2022",
+        dateEnded: "PRESENT",
+        descriptionPoints: [
+            "created full stack applications",
+            "created full stack applications",
+            "created full stack applications"
+        ]
+    },
+    {
+        jobPosition: "Internship",
+        companyName: "Credot",
+        dateStarted: "September 2021",
+        dateEnded: "January 2022",
+        descriptionPoints: [
+            "Involved in projects as an intern",
+            "created full stack applications"
+        ]
+    }
+]
