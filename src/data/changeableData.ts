d4b38f447a2b6d82d0f377dd70850eb1b4ca0386
bijish-personal <bijish_js@outlook.com>
2023-03-16 23:12:28 +0530

    desktop version created, should be made responsive

--- src/data/changeableData.ts
+++ src/data/changeableData.ts
@@ -1,35 +1,116 @@
 export const experienceData = [
     {
         jobPosition: "Team Lead",
-        companyName: "Credot",
+        companyName: "Credot LLP",
+        companyLink: "https://credot.co",
         dateStarted: "October 2022",
         dateEnded: "PRESENT",
         descriptionPoints: [
-            "Lead a team, created full stack applications",
-            "Lead a team",
-            "Lead a team",
-            "Lead a team",
+            "Proven track record of leadership and team management, with experience leading and mentoring teams of developers to successful project outcomes, including front-end development using React and state management library Recoil.",
+
+            "Skilled in back-end development, with experience building GraphQL APIs using Node.js, Express, and MongoDB.",
+
+            "Strong familiarity with modern web development technologies and frameworks, including Apollo Client, Redux, Recoil, and Git."
+
         ]
     },
     {
-        jobPosition: "Full stack developer",
-        companyName: "Credot",
+        jobPosition: "Full Stack Developer",
+        companyName: "Credot LLP",
+        companyLink: "https://credot.co",
         dateStarted: "May 2022",
         dateEnded: "PRESENT",
         descriptionPoints: [
-            "created full stack applications",
-            "created full stack applications",
-            "created full stack applications"
+            "Experienced full-stack developer with expertise in MERN stack, including extensive experience with back-end development using GraphQL, Node.js, Express, and MongoDB, and front-end development using React and state management library Recoil.",
+            "Proven experience building e-commerce websites, warranty management and product registration systems, logistic shipment status applications, and other complex systems using a variety of technologies.",
+            "Strong familiarity with modern web development frameworks and libraries, including Apollo Client, Redux, and other popular state management libraries."
+
         ]
     },
     {
         jobPosition: "Internship",
-        companyName: "Credot",
+        companyName: "Credot LLP",
+        companyLink: "https://credot.co",
         dateStarted: "September 2021",
         dateEnded: "January 2022",
         descriptionPoints: [
-            "Involved in projects as an intern",
-            "created full stack applications"
+
+
+            "Internship experience in full-stack web development, working on front-end projects for property booking sites and accounting firm websites.",
+
+            "Experience working with the MERN stack, including React, Node.js, Express, and MongoDB.",
+
+            "Demonstrated ability to collaborate effectively with team members, designers, and stakeholders to deliver high-quality projects.",
+
+            "Proven experience working in a fast-paced, dynamic environment and adapting to changing requirements and timelines."
         ]
     }
 ]
+
+export const skills = [
+    {
+        name: "React JS",
+        progress: "80%",
+        icon: "../images/React-js.png"
+    },
+    {
+        name: "Next JS",
+        progress: "80%",
+        icon: "../images/Next-js.png"
+    },
+    {
+        name: "GraphQL",
+        progress: "80%",
+        icon: "../images/Graphql.png"
+    },
+    {
+        name: "Node JS",
+        progress: "80%",
+        icon: "../images/Node-js.png"
+    }, {
+        name: "Express JS",
+        progress: "80%",
+        icon: "../images/Express-js.png"
+    }
+    , {
+        name: "MongoDB",
+        progress: "80%",
+        icon: "../images/Mongodb.svg"
+    },
+    , {
+        name: "TypeScript",
+        progress: "80%",
+        icon: "../images/Typescript.png"
+    },
+    , {
+        name: "Astro",
+        progress: "80%",
+        icon: "../images/astro.png"
+    },
+    , {
+        name: "Recoil",
+        progress: "80%",
+        icon: "../images/recoil.png"
+    },
+]
+
+export const projectData = [
+    {
+        title: "Project 1",
+        description: "This is project 1 description.",
+        image: "../images/placeholder.svg"
+    },
+    {
+        title: "Project 2",
+        description: "This is project 2 description.",
+        image: "../images/placeholder.svg"
+    },
+    {
+        title: "Project 3",
+        description: "This is project 3 description.",
+        image: "../images/placeholder.svg"
+    },
+
+]
+
+export const aboutContent = "I'm a skilled web developer with expertise in both front-end and back-end technologies. With years of experience building complex web applications for various industries, including e-commerce, property booking, and warranty management. I am committed to staying up-to-date with the latest web development technologies and frameworks. I have a passion for problem-solving and a track record of delivering successful web applications, and I'm always eager to tackle new challenges and take on new projects."
