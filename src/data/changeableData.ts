261366a41fffe9924a86ed6591f39434ba0e86c2
bijish-personal <bijish_js@outlook.com>
2023-05-05 20:25:27 +0530

    updation of projects

--- src/data/changeableData.ts
+++ src/data/changeableData.ts
@@ -4,13 +4,13 @@ export const experienceData = [
         companyName: "Credot LLP",
         companyLink: "https://credot.co",
         dateStarted: "October 2022",
-        dateEnded: "PRESENT",
+        dateEnded: "March 2023",
         descriptionPoints: [
             "Proven track record of leadership and team management, with experience leading and mentoring teams of developers to successful project outcomes, including front-end development using React and state management library Recoil.",
 
             "Skilled in back-end development, with experience building GraphQL APIs using Node.js, Express, and MongoDB.",
 
-            "Strong familiarity with modern web development technologies and frameworks, including Apollo Client, Redux, Recoil, and Git."
+            "Strong familiarity with modern web development technologies and frameworks, including Apollo Client, Recoil, and Git."
 
         ]
     },
@@ -19,11 +19,11 @@ export const experienceData = [
         companyName: "Credot LLP",
         companyLink: "https://credot.co",
         dateStarted: "May 2022",
-        dateEnded: "PRESENT",
+        dateEnded: "March 2023",
         descriptionPoints: [
             "Experienced full-stack developer with expertise in MERN stack, including extensive experience with back-end development using GraphQL, Node.js, Express, and MongoDB, and front-end development using React and state management library Recoil.",
             "Proven experience building e-commerce websites, warranty management and product registration systems, logistic shipment status applications, and other complex systems using a variety of technologies.",
-            "Strong familiarity with modern web development frameworks and libraries, including Apollo Client, Redux, and other popular state management libraries."
+            "Strong familiarity with modern web development frameworks and libraries, including Apollo Client, and other popular state management libraries."
 
         ]
     },
@@ -96,21 +96,23 @@ export const skills = [
 
 export const projectData = [
     {
-        title: "Project 1",
-        description: "This is project 1 description.",
-        image: "/images/placeholder.svg"
+        title: "Widget Fusion",
+        description: "Transform your website with our magical no-code tool! Effortlessly add custom widgets without any coding knowledge required.",
+        image: "/images/widgetify-mockup.png",
+        link: "https://widgetfusion.com"
     },
     {
-        title: "Project 2",
-        description: "This is project 2 description.",
-        image: "/images/placeholder.svg"
+        title: "GPT in Terminal",
+        description: "GPT-in-Terminal is a versatile and user-friendly command-line tool built with Node.js that allows developers to interact seamlessly with OpenAI's powerful GPT-3 language model.",
+        image: "/images/chat-gpt.png",
+        link: "https://www.npmjs.com/package/gpt-in-terminal"
     },
-    {
-        title: "Project 3",
-        description: "This is project 3 description.",
-        image: "/images/placeholder.svg"
-    },
+    // {
+    //     title: "Project 3",
+    //     description: "This is project 3 description.",
+    //     image: "/images/placeholder.svg"
+    // },
 
 ]
-
+export const devDotToUrl = "/images/dev-dot-to.webp"
 export const aboutContent = "I'm a skilled web developer with expertise in both front-end and back-end technologies. With years of experience building complex web applications for various industries, including e-commerce, property booking, and warranty management. I am committed to staying up-to-date with the latest web development technologies and frameworks. I have a passion for problem-solving and a track record of delivering successful web applications, and I'm always eager to tackle new challenges and take on new projects."
