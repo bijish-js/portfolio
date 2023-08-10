358d43dfda6c4068781a2250848cd5c9e414bd23
Bijish O B <bijish-js@outlook.com>
2023-08-10 13:09:09 +0530

    added education and certification

--- src/data/changeableData.ts
+++ src/data/changeableData.ts
@@ -1,118 +1,163 @@
 export const experienceData = [
-    {
-        jobPosition: "Team Lead",
-        companyName: "Credot LLP",
-        companyLink: "https://credot.co",
-        dateStarted: "October 2022",
-        dateEnded: "March 2023",
-        descriptionPoints: [
-            "Proven track record of leadership and team management, with experience leading and mentoring teams of developers to successful project outcomes, including front-end development using React and state management library Recoil.",
+  {
+    jobPosition: "Freelance Consultant & Full Stack Developer ",
+    companyName: "Self employed",
+    companyLink: "",
+    dateStarted: "April 2023",
+    dateEnded: "Present",
+    descriptionPoints: [
+      "Leveraging expertise in full stack development to deliver robust and user-centric web applications.",
+      "Independently providing consultation services to clients, offering tailored solutions to their technical challenges.",
+      "Collaborating closely with clients to understand requirements, architect solutions, and implement effective strategies.",
+      "Developing and optimizing web applications using the MERN stack, GraphQL, and other relevant technologies.",
+      "Demonstrating strong problem-solving skills and an ability to adapt to diverse project scopes and industries.",
+      ,
+    ],
+  },
+  {
+    jobPosition: "Team Lead",
+    companyName: "Credot LLP",
+    companyLink: "https://credot.co",
+    dateStarted: "October 2022",
+    dateEnded: "March 2023",
+    descriptionPoints: [
+      "Proven track record of leadership and team management, with experience leading and mentoring teams of developers to successful project outcomes, including front-end development using React and state management library Recoil.",
 
-            "Skilled in back-end development, with experience building GraphQL APIs using Node.js, Express, and MongoDB.",
+      "Skilled in back-end development, with experience building GraphQL APIs using Node.js, Express, and MongoDB.",
 
-            "Strong familiarity with modern web development technologies and frameworks, including Apollo Client, Recoil, and Git."
+      "Strong familiarity with modern web development technologies and frameworks, including Apollo Client, Recoil, and Git.",
+    ],
+  },
+  {
+    jobPosition: "Full Stack Developer",
+    companyName: "Credot LLP",
+    companyLink: "https://credot.co",
+    dateStarted: "May 2022",
+    dateEnded: "March 2023",
+    descriptionPoints: [
+      "Experienced full-stack developer with expertise in MERN stack, including extensive experience with back-end development using GraphQL, Node.js, Express, and MongoDB, and front-end development using React and state management library Recoil.",
+      "Proven experience building e-commerce websites, warranty management and product registration systems, logistic shipment status applications, and other complex systems using a variety of technologies.",
+      "Strong familiarity with modern web development frameworks and libraries, including Apollo Client, and other popular state management libraries.",
+    ],
+  },
+  {
+    jobPosition: "Internship",
+    companyName: "Credot LLP",
+    companyLink: "https://credot.co",
+    dateStarted: "September 2021",
+    dateEnded: "January 2022",
+    descriptionPoints: [
+      "Internship experience in full-stack web development, working on front-end projects for property booking sites and accounting firm websites.",
 
-        ]
-    },
-    {
-        jobPosition: "Full Stack Developer",
-        companyName: "Credot LLP",
-        companyLink: "https://credot.co",
-        dateStarted: "May 2022",
-        dateEnded: "March 2023",
-        descriptionPoints: [
-            "Experienced full-stack developer with expertise in MERN stack, including extensive experience with back-end development using GraphQL, Node.js, Express, and MongoDB, and front-end development using React and state management library Recoil.",
-            "Proven experience building e-commerce websites, warranty management and product registration systems, logistic shipment status applications, and other complex systems using a variety of technologies.",
-            "Strong familiarity with modern web development frameworks and libraries, including Apollo Client, and other popular state management libraries."
+      "Experience working with the MERN stack, including React, Node.js, Express, and MongoDB.",
 
-        ]
-    },
-    {
-        jobPosition: "Internship",
-        companyName: "Credot LLP",
-        companyLink: "https://credot.co",
-        dateStarted: "September 2021",
-        dateEnded: "January 2022",
-        descriptionPoints: [
+      "Demonstrated ability to collaborate effectively with team members, designers, and stakeholders to deliver high-quality projects.",
 
-
-            "Internship experience in full-stack web development, working on front-end projects for property booking sites and accounting firm websites.",
-
-            "Experience working with the MERN stack, including React, Node.js, Express, and MongoDB.",
-
-            "Demonstrated ability to collaborate effectively with team members, designers, and stakeholders to deliver high-quality projects.",
-
-            "Proven experience working in a fast-paced, dynamic environment and adapting to changing requirements and timelines."
-        ]
-    }
-]
+      "Proven experience working in a fast-paced, dynamic environment and adapting to changing requirements and timelines.",
+    ],
+  },
+];
 
 export const skills = [
-    {
-        name: "React JS",
-        progress: "80%",
-        icon: "/images/React-js.png"
-    },
-    {
-        name: "Next JS",
-        progress: "80%",
-        icon: "/images/Next-js.png"
-    },
-    {
-        name: "GraphQL",
-        progress: "80%",
-        icon: "/images/Graphql.png"
-    },
-    {
-        name: "Node JS",
-        progress: "80%",
-        icon: "/images/Node-js.png"
-    }, {
-        name: "Express JS",
-        progress: "80%",
-        icon: "/images/Express-js.png"
-    }
-    , {
-        name: "MongoDB",
-        progress: "80%",
-        icon: "/images/Mongodb.svg"
-    },
-    , {
-        name: "TypeScript",
-        progress: "80%",
-        icon: "/images/Typescript.png"
-    },
-    , {
-        name: "Astro",
-        progress: "80%",
-        icon: "/images/astro.png"
-    },
-    , {
-        name: "Recoil",
-        progress: "80%",
-        icon: "/images/recoil.png"
-    },
-]
+  {
+    name: "React JS",
+    progress: "80%",
+    icon: "/images/React-js.png",
+  },
+  {
+    name: "Next JS",
+    progress: "80%",
+    icon: "/images/Next-js.png",
+  },
+  {
+    name: "GraphQL",
+    progress: "80%",
+    icon: "/images/Graphql.png",
+  },
+  {
+    name: "Node JS",
+    progress: "80%",
+    icon: "/images/Node-js.png",
+  },
+  {
+    name: "Express JS",
+    progress: "80%",
+    icon: "/images/Express-js.png",
+  },
+  {
+    name: "MongoDB",
+    progress: "80%",
+    icon: "/images/Mongodb.svg",
+  },
+  ,
+  {
+    name: "TypeScript",
+    progress: "80%",
+    icon: "/images/Typescript.png",
+  },
+  ,
+  {
+    name: "Astro",
+    progress: "80%",
+    icon: "/images/astro.png",
+  },
+  ,
+  {
+    name: "Recoil",
+    progress: "80%",
+    icon: "/images/recoil.png",
+  },
+];
 
 export const projectData = [
-    {
-        title: "Widget Fusion",
-        description: "Transform your website with our magical no-code tool! Effortlessly add custom widgets without any coding knowledge required.",
-        image: "/images/widgetify-mockup.png",
-        link: "https://widgetfusion.com"
-    },
-    {
-        title: "GPT in Terminal",
-        description: "GPT-in-Terminal is a versatile and user-friendly command-line tool built with Node.js that allows developers to interact seamlessly with OpenAI's powerful GPT-3 language model.",
-        image: "/images/chat-gpt.png",
-        link: "https://www.npmjs.com/package/gpt-in-terminal"
-    },
-    // {
-    //     title: "Project 3",
-    //     description: "This is project 3 description.",
-    //     image: "/images/placeholder.svg"
-    // },
+  {
+    title: "Widget Fusion",
+    description:
+      "Transform your website with our magical no-code tool! Effortlessly add custom widgets without any coding knowledge required.",
+    image: "/images/widgetify-mockup.png",
+    link: "https://widgetfusion.com",
+  },
+  {
+    title: "GPT in Terminal",
+    description:
+      "GPT-in-Terminal is a versatile and user-friendly command-line tool built with Node.js that allows developers to interact seamlessly with OpenAI's powerful GPT-3 language model.",
+    image: "/images/chat-gpt.png",
+    link: "https://www.npmjs.com/package/gpt-in-terminal",
+  },
+  // {
+  //     title: "Project 3",
+  //     description: "This is project 3 description.",
+  //     image: "/images/placeholder.svg"
+  // },
+];
 
-]
-export const devDotToUrl = "/images/dev-dot-to.webp"
-export const aboutContent = "I'm a skilled web developer with expertise in both front-end and back-end technologies. With years of experience building complex web applications for various industries, including e-commerce, property booking, and warranty management. I am committed to staying up-to-date with the latest web development technologies and frameworks. I have a passion for problem-solving and a track record of delivering successful web applications, and I'm always eager to tackle new challenges and take on new projects."
+export const EducationData = [
+  {
+    educationPosition: "Master of Computer Applications (MCA)",
+    authority: "University of Calicut",
+    dateStarted: "July 2018",
+    dateEnded: "Decemeber 2021",
+    descriptionPoints: [],
+  },
+
+  {
+    educationPosition: "Bachelor of Science (BSc) in Computer Science",
+    authority: "University of Calicut",
+    dateStarted: "July 2015",
+    dateEnded: "April 2018",
+    descriptionPoints: [],
+  },
+];
+
+export const CertificationData = [
+  {
+    certificationPosition: "Assistant Professorship in Computer Science",
+    authority: "UGC - NET",
+    dateStarted: "Decemeber 2019",
+    dateEnded: "Present",
+    descriptionPoints: [],
+  },
+];
+export const devDotToUrl = "/images/dev-dot-to.webp";
+export const aboutContent =
+  "I'm a skilled web developer with expertise in both front-end and back-end technologies. With years of experience building complex web applications for various industries, including e-commerce, property booking, and warranty management. I am committed to staying up-to-date with the latest web development technologies and frameworks. I have a passion for problem-solving and a track record of delivering successful web applications, and I'm always eager to tackle new challenges and take on new projects.";
